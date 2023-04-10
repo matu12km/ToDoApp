605a1476c69811c44a8c7e5285f6448d30c7e497
matu12km <71113180+matu12km@users.noreply.github.com>
2023-04-11 08:41:06 +0900

    ヘッダー修正

--- my-todo-app/src/component/Header.tsx
+++ my-todo-app/src/component/Header.tsx
@@ -5,12 +5,12 @@ export const Header = () => {
         <span className="hidden md:block">TODO List</span>
       </div>
       <div className="flex justify-between items-center h-14 bg-blue-800 dark:bg-gray-800 header-right">
-        <div className="bg-white rounded flex items-center w-full max-w-xl mr-4 p-2 shadow-sm border border-gray-200">
+        {/* <div className="bg-white rounded flex items-center w-full max-w-xl mr-4 p-2 shadow-sm border border-gray-200">
           <button className="outline-none focus:outline-none">
             <svg className="w-5 text-gray-600 h-5 cursor-pointer" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
           </button>
           <input type="search" name="search" id="searchBox" placeholder="検索" className="w-full pl-3 text-sm text-black outline-none focus:outline-none bg-transparent" />
-        </div>
+        </div> */}
         {/* <ul className="flex items-center">
           <li>
             <button aria-hidden="true" className="group p-2 transition-colors duration-200 rounded-full shadow-md bg-blue-200 hover:bg-blue-200 dark:bg-gray-50 dark:hover:bg-gray-200 text-gray-900 focus:outline-none">
